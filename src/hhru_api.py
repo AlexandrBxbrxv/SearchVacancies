5fd7d1ee1bf4fd5722d6b36e761c586b6c4b810f
AlexandrBxbrxv <g1026@bk.ru>
2024-07-03 03:36:02 +0500

    update hhru_api.py

--- src/hhru_api.py
+++ src/hhru_api.py
@@ -5,12 +5,6 @@ import requests
 from src.parser import Parser
 
 
-path_to_project = path.abspath(__file__)[:-15]
-file_name = r'data\loaded_data.json'
-# Путь до файла loaded_data.json
-file_path = f'{path_to_project}{file_name}'
-
-
 class HHruAPI(Parser):
     """
     Класс для работы с API HeadHunter
@@ -24,25 +18,10 @@ class HHruAPI(Parser):
         super().__init__(file_worker)
 
     def load_vacancies(self, keyword):
+        """Загружает с сайта api.hh.ru вакансии и складывает их в список vacancies"""
         self.params['text'] = keyword  # ['job']
         while self.params.get('page') != 10:
             response = requests.get(self._url, headers=self._headers, params=self.params)
             vacancies = response.json()['items']
             self.vacancies.extend(vacancies)
             self.params['page'] += 1
-
-        # with open(file=file_path, mode='wt', encoding='UTF-8') as file:
-        #     file.write(json.dumps(self.vacancies))
-
-
-# hh_api = HHruAPI('')
-# hh_api.load_vacancies('Java')
-
-# with open(file=file_path, mode='rt', encoding='UTF-8') as file:
-#     data = json.loads(file.readline())
-#
-# index = 1
-# for item in data:
-#     print(f'{index}: {item.get('name')} {item.get('salary')}')
-#     index += 1
-
