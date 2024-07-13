5af1a5bfe1589abe2bb3c02eab8d747f147f7d27
AlexandrBxbrxv <g1026@bk.ru>
2024-07-13 16:07:38 +0500

    add index

--- src/vacancy.py
+++ src/vacancy.py
@@ -5,18 +5,20 @@ class Vacancy:
     pay: int
     currency: str
     working_days: str
-    # description: str
-    # employment: str
-    # area: str
+    index: int
+
+    index = -1
 
     def __init__(self, name, salary, working_days, link):
         if salary is None:
             salary = {}
         self.name = name if name else 'Название не указано'
         self.currency = salary.get('currency') if salary.get('currency') else 'Валюта не указана'
-        self.pay = salary.get('from') if salary.get('from') else 0
+        self.pay = salary.get('from') if salary.get('from') else 'Зарплата не указана'
         self.working_days = working_days if working_days else 'График не указан'
         self.link = link if link else 'Ссылка не указана'
+        Vacancy.index += 1
+        self.index = Vacancy.index
 
     def __repr__(self):
         """Представление для отладки"""
@@ -44,25 +46,3 @@ class Vacancy:
             else:
                 return f'На второй работе зарплата ниже на {abs(difference)} {self.currency}'
         raise TypeError('Сравнивать можно только с экземплярами класса Vacancy')
-
-
-if __name__ == '__main__':
-
-    sal1 = {'currency': None, 'from': 5000}
-    sal2 = {'currency': 'RUB', 'from': 7000}
-    vac1 = Vacancy('pudge', sal1, 'ПН-ПТ', 'link//link231')
-    vac2 = Vacancy('rudge', sal2, 'ПН-ПТ', 'link//link231')
-    vac3 = Vacancy('rudge', None, 'ПН-ПТ', 'link//link231')
-    print(vac1)
-    print(vac2)
-    print(vac1.compare_pay(vac2))
-
-
-"Создать класс для работы с вакансиями. В этом классе самостоятельно определить атрибуты,"
-"такие как название вакансии, ссылка на вакансию, зарплата, краткое описание или требования и т. п."
-"(всего не менее четырех атрибутов)."
-"Класс должен поддерживать методы сравнения вакансий между собой по зарплате и валидировать данные,"
-"которыми инициализируются его атрибуты.)"
-
-"Способами валидации данных может быть проверка, указана или нет зарплата."
-"В этом случае выставлять значение зарплаты 0 или «Зарплата не указана» в зависимости от структуры класса."
