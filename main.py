8febddc3ea0879801c96eb7e170d4f9fca5e3045
AlexandrBxbrxv <g1026@bk.ru>
2024-07-15 05:51:40 +0500

    can load from api, save and print sorted by pay

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
-from src.hhru_api import HHruAPI
-from src.vacancy import Vacancy
-from src.file_worker_json import FileWorkerJson
+from src.functions import user_interaction
 
-hh = HHruAPI()
-hh.load_vacancies('Python')
+if __name__ == '__main__':
 
+    with open('data/saved_vacancies.json', 'w') as f:
+        f.write('')
 
-vacancies = Vacancy.cast_to_object_list(hh.vacancies)
-print(len(vacancies))
-print(vacancies)
-
-file_worker = FileWorkerJson()
-file_worker.add_vacancy(vacancies[0])
-
-# # Создание экземпляра класса для работы с API сайтов с вакансиями
-# hh_api = HeadHunterAPI()
-#
-# # Получение вакансий с hh.ru в формате JSON
-# hh_vacancies = hh_api.get_vacancies("Python")
-#
-# # Преобразование набора данных из JSON в список объектов
-# vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
-#
-# # Пример работы конструктора класса с одной вакансией
-# vacancy = Vacancy("Python Developer", "", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
-#
-# # Сохранение информации о вакансиях в файл
-# json_saver = JSONSaver()
-# json_saver.add_vacancy(vacancy)
-# json_saver.delete_vacancy(vacancy)
+    user_interaction()
