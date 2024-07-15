57836785fa154fcd8bc560f3c801f15e3e8c1f50
AlexandrBxbrxv <g1026@bk.ru>
2024-07-15 06:59:55 +0500

    redo link to alternate_url

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,7 +1,7 @@
 class Vacancy:
     """Класс для работы с вакансиями"""
     name: str
-    link: str
+    alternate_url: str
     pay: int
     currency: str
     working_days: str
@@ -9,24 +9,24 @@ class Vacancy:
 
     index = -1
 
-    def __init__(self, name, salary, working_days, link):
+    def __init__(self, name, salary, working_days, alternate_url):
         if salary is None:
             salary = {}
         self.name = name if name else 'Название не указано'
         self.currency = salary.get('currency') if salary.get('currency') else 'Валюта не указана'
         self.pay = salary.get('from') if salary.get('from') else 0
         self.working_days = working_days if working_days else 'График не указан'
-        self.link = link if link else 'Ссылка не указана'
+        self.alternate_url = alternate_url if alternate_url else 'Ссылка не указана'
         Vacancy.index += 1
         self.index = Vacancy.index
 
     def __repr__(self):
         """Представление для отладки"""
-        return f"{self.__class__.__name__}('{self.name}', {self.pay}, '{self.working_days}', '{self.link}')"
+        return f"{self.__class__.__name__}('{self.name}', {self.pay}, '{self.working_days}', '{self.alternate_url}')"
 
     def __str__(self):
         """Представление для пользователя"""
-        return f'{self.name}, {self.pay} {self.currency}, {self.working_days}, {self.link}'
+        return f'{self.name}, {self.pay} {self.currency}, {self.working_days}, {self.alternate_url}'
 
     @classmethod
     def cast_to_object_list(cls, items):
